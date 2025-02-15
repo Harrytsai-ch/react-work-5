5916fe78a39cfac084823bdba4a68767f912f64a
HarryTsai <sleepingturtle990866@gmail.com>
2025-02-15 14:47:58 +0800

    finished

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css' // 載入 bootstrap 核心樣式  
 import App from './App.jsx'
+import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
